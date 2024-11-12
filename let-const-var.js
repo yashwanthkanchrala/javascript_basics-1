2c6b457169a8135045eefa7c07c26228b5994a2f
yashwanthkanchrala <yashwanthkancharla17@gmail.com>
2024-11-12 16:50:02 +0530

    my third commit

--- let-const-var.js
+++ let-const-var.js
@@ -1,2 +1,15 @@
-true;
-console.log(true);
+//true;
+//console.log(true);
+
+let age = 30;
+age = 31;
+
+const birthyear = 1996;
+//birthyear = 1995;
+//const job;
+
+var job = "programmer";
+job = "engineer";
+
+lastname = "yashwanth";
+console.log(lastname);
