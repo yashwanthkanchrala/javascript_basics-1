c82f7a0fbc2a762c0f1531f9db4cfb0e1a785813
yashwanthkanchrala <yashwanthkancharla17@gmail.com>
2024-11-12 17:46:20 +0530

    my third commit

---
+++ basicoperators.js
@@ -0,0 +1,27 @@
+const age = 2037;
+const ageYash = age - 1996;
+const ageSai = age - 2014;
+console.log(ageYash, ageSai);
+
+console.log(ageYash * 2, ageSai / 2);
+
+console.log(2 ** 3);
+
+const firstName = "yashwanth";
+const lastName = "kancharla";
+console.log(firstName + " " + lastName);
+console.log(firstName + lastName);
+//ASSIGNMENT OPERATORS
+let x = 10 + 5;
+x += 10;
+x++;
+x--;
+x *= 4;
+x **= 2;
+x /= 4;
+console.log(x);
+// COMPARISION OPERATORS
+console.log(ageYash > ageSai);
+console.log(ageYash < ageSai);
+//<,>,>=,<=
+console.log(ageSai >= 17);
