63416db8d9a22368e9672d32737943f430d5e9a3
yashwanthkanchrala <yashwanthkancharla17@gmail.com>
2024-11-12 15:58:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+/*
+let js = "amazing";
+console.log("jonas");
+
+let firstName = "kancharla";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+*/
+true;
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "yashwanth");
+javascriptIsFun = "yes";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1996;
+console.log(typeof year);
+
+console.log(typeof null);
